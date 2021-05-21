61446860cc4e05207f4806abda9c978d4240d20a
burçak kasap <burcak_kasap92@hotmail.com>
2021-05-21 18:04:45 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyList<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public MyList()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempkey = keys;
+            Tvalue[] tempvalue = values;
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length+1];
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                keys[i] = tempkey[i];
+                values[i] = tempvalue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Length { get { return keys.Length; } }
+        public Tkey[] _key { get { return keys; } }
+        public Tvalue[] _value { get { return values; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> myList = new MyList<int, string>();
+            myList.Add(1, "aa");
+            Console.WriteLine(myList.Length);
+            myList.Add(2, "bb");
+            Console.WriteLine(myList.Length);
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine(myList._key[i] + "" + myList._value[i]);
+            }
+        }
+    }
+}
